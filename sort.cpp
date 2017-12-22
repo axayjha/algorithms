34e502e87a3240b4adf0a9298803aa5d7eadefae
Akshay <akshayjha@live.in>
2017-12-22 19:49:03 +0530

    revised code

--- sort.cpp
+++ sort.cpp
@@ -1,42 +1,65 @@
-// proj2.cpp : Defines the entry point for the console application.
-//
-
+/*
+	Generic randomized quicksort
+	Akshay Anand
+	29/11/17
+*/
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <stdint.h>
+#define SIZE(array) sizeof(array)/sizeof(array[0])
+
 using namespace std;
 
-template  <typename whatever> void sort(whatever *, int ,  bool (*cmp)(whatever, whatever, const string), const string order="ascending");
+enum { ASCENDING = 0xbabe, DESCENDING = 0xface };
+typedef uint64_t sorting;
+
+template <typename whatever> void sort(whatever *, size_t ,
+										 bool (*cmp)(whatever, whatever, sorting),
+										 sorting order = ASCENDING);
+
 template <typename whatever> void swap(whatever *, whatever *);
-template <typename whatever> bool cmp(whatever , whatever , const string);
+template <typename whatever> bool cmp(whatever , whatever , sorting);
+
 
 int main(int argc, char* argv[])
-{
-	string s[] = {"aksha", "aa", "sad"};
-	sort(s, 3, cmp);
-	for(int i=0; i<3; i++)
-		cout << s[i] << " ";
+{	
+	int a[]={1,3,45,2,6,2};
+	sort(a, SIZE(a), cmp, DESCENDING);
+	for(int i=0; i<SIZE(a); i++)
+		cout << a[i] << " ";
 	cout << endl;
 	return 0;
 }
 
-template  <typename whatever> void sort(whatever *a, int n,  bool (*cmp)(whatever, whatever, const string), const string order)
-{
+template <typename whatever> void sort(
+		whatever *array, size_t n, 
+		bool (*cmp)(whatever value1, whatever value2, sorting order), 
+		sorting order){
+
+	if(n<=1) return;
+	int last = 0;
+
+	swap(&array[0], &array[rand()%n]);
 	for(int i=0; i<n; i++)
-		for(int j=0; j<n-i-1; j++)
-			if(cmp(a[j], a[j+1], order))
-				swap(a+j, a+j+1);
-
+		if(cmp(array[0] , array[i], order))
+			swap(&array[++last], &array[i]);
+	swap(&array[0], &array[last]);
+	
+	sort(array, last, cmp, order);
+	sort(array+last+1, n-last-1, cmp, order);	
 }
 
-template <typename whatever> void swap(whatever *a, whatever *b){
-	whatever temp = *a;
-	*a = *b;
-	*b = temp;
+template <typename whatever> void swap(
+			whatever *value1, whatever *value2) {
+	whatever temp = *value1;
+	*value1 = *value2;
+	*value2 = temp;
 }
 
-template <typename whatever> bool cmp(whatever a, whatever b, const string order){
-	if(order== "ascending")
-		return a>b;
-	return a<b;
+template <typename whatever> bool cmp(
+			whatever value1, whatever value2, sorting order){
+	if(order == ASCENDING)
+		return value1 > value2;
+	return value1 < value2;
 }
