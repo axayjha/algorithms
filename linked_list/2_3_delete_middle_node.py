4631b0acf703d46f24f52bb38d020a1d8aa165e5
CSE16 <jha.axay@gmail.com>
2020-10-30 16:14:21 +0530

    Add files via upload

---
+++ linked_list/2_3_delete_middle_node.py
@@ -0,0 +1,15 @@
+"""
+Q2.3: Implement an algorithm to delete a node in the middle (i.e., any node but the first
+and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+Example: 
+Input: the node c from the linked list a->b->c->d->e->f
+Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+"""
+
+def deleteNode(node):
+        if not node or not node.next: return False 
+
+        node.val = node.next.val 
+        node.next = node.next.next 
+
+        return True
